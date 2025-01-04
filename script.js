a458a287329752eaf75c7ed3f19763010d417d73
Rahul Roy <royrahulroy2004@gmail.com>
2025-01-05 00:58:37 +0530

    Initialized Files

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const cards = document.querySelectorAll(".project-card");
+    cards.forEach((card) => {
+        card.addEventListener("mousemove", (e) => {
+            const rect = card.getBoundingClientRect();
+            const x = e.clientX - rect.left;
+            const y = e.clientY - rect.top;
+
+            card.style.setProperty("--mouse-x", `${x}px`);
+            card.style.setProperty("--mouse-y", `${y}px`);
+        });
+    });
+});
